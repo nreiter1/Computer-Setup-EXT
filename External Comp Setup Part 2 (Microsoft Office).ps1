d772bbb1d4f7eecbe0362820269bca69a0a8b55b
Nicolas Reiter <59969730+nreiter1@users.noreply.github.com>
2023-03-20 15:55:04 -0500

    Update External Comp Setup Part 2 (Microsoft Office).ps1

--- External Comp Setup Part 2 (Microsoft Office).ps1
+++ External Comp Setup Part 2 (Microsoft Office).ps1
@@ -12,14 +12,14 @@ while($officeInstallWhile -eq 1)
     {
         Read-Host -Prompt "Refer to the Microsoft Office Remover Tool IF this OS came with this computer and you HAVEN'T done so.`nWhen done, press enter to install office"
         Write-Output $install2021Standard
-        .\setup.exe /configure "F:\ComputerSetup\Applications\Office 2021 LTSC Std and Pro Plus\Office\Configuration-GC-SHI-Std-64.xml"
+        .\setup.exe /configure "F:\ComputerSetup\Applications\Office 2021 LTSC Std and Pro Plus\Office\Configuration-GC-SHI-Std-64.xml" #NOT INCLUDED IN GIT REPOSITORY FOR SECURITY REASONS
         $officeInstallWhile = 0
     }
     elseif($officeInstall -eq "P")#21 Pro Plus 64-Bit
     {
         Read-Host -Prompt "Refer to the Microsoft Remover Tool IF this OS came with this computer and you HAVEN'T done so.`nWhen done, press enter to install office"
         Write-Output $install2021ProfessionalPlus
-        .\setup.exe /configure "F:\ComputerSetup\Applications\Office 2021 LTSC Std and Pro Plus\Office\Configuration-GC-SHI-ProPlus-64.xml"
+        .\setup.exe /configure "F:\ComputerSetup\Applications\Office 2021 LTSC Std and Pro Plus\Office\Configuration-GC-SHI-ProPlus-64.xml" #NOT INCLUDED IN GIT REPOSITORY FOR SECURITY REASONS
         $officeInstallWhile = 0
     }
     elseif($officeInstall -eq "N") #No Office Installed
